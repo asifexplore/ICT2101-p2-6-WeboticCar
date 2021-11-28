bd556e08474e8102e20bda2757c8026562c9d5f3
Greenbonee <nadzim1998@gmail.com>
2021-11-28 22:31:28 +0800

    highscore table database

--- init.py
+++ init.py
@@ -30,6 +30,43 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.id
 
+class Map(db.Model):
+    map_id = db.Column(db.Integer, primary_key=True)
+    one = db.Column(db.String(10))
+    two = db.Column(db.String(10))
+    three = db.Column(db.String(10))
+    four = db.Column(db.String(10))
+    five = db.Column(db.String(10))
+    six = db.Column(db.String(10))
+    seven = db.Column(db.String(10))
+    eight = db.Column(db.String(10))
+    nine = db.Column(db.String(10))
+    ten = db.Column(db.String(10))
+    name = db.Column(db.String(20))
+    pin = db.Column(db.Integer, nullable=True)
+
+    def __init__(self, one, two, three, four, five, six, seven, eight, nine, ten, name):
+        self.one = one
+        self.two = two
+        self.three = three
+        self.four = four
+        self.five = five
+        self.six = six
+        self.seven = seven
+        self.eight = eight
+        self.nine = nine
+        self.ten = ten
+        self.name = name
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Map %r>' % self.map_id
+
+    def setPIN(self, pin: int) -> bool:
+        self.pin = pin
+        return True
+
 class Cars(db.Model):
     car_id = db.Column(db.Integer, primary_key=True)
     car_pass = db.Column(db.String(80))
@@ -43,9 +80,9 @@ class Cars(db.Model):
         self.token = Null
 
 class Highscore(db.Model):
-    score_id = db.column(db.Integer, primary_key=True, unique=True)
-    map_id = db.column(db.Integer)
-    name = db.column(db.String(10))
+    score_id = db.Column(db.Integer, primary_key=True, unique=True)
+    map_id = db.Column(db.Integer)
+    name = db.Column(db.String(10))
     score = db.Column(db.Integer)
 
 db.create_all()
