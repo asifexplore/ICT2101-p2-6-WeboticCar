939c067a0c046cd48930f80b98b7a51ad9caf6a1
marven-lim <74282240+marven-lim@users.noreply.github.com>
2021-12-01 15:53:53 +0800

    Integrated Properly
    Removed improper placement of codes.
    Cleaned up inefficient codes.

--- app.py
+++ app.py
@@ -1,88 +1,15 @@
 from flask import render_template, url_for, request, redirect
 from init import app
-from controller.userManagement import isTeacher
-from controller.mapControl import createMap, isValidMap, makeChallenge
-
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-from flask_sslify import SSLify
-from sqlalchemy.sql.elements import Null
-
-from controller import mapControl
-import sqlite3
-
-app = Flask(__name__)
-app.secret_key = "wow_so_secret!"
-
-context = ('cert.pem', 'key.pem')
-sslify = SSLify(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
-# db.app = app
-
-def commit():
-    db.session.commit()
-    return
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(80))
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return '<User %r>' % self.id
-
-
-class Map(db.Model):
-    map_id = db.Column(db.Integer, primary_key=True)
-    one = db.Column(db.String(10))
-    two = db.Column(db.String(10))
-    three = db.Column(db.String(10))
-    four = db.Column(db.String(10))
-    five = db.Column(db.String(10))
-    six = db.Column(db.String(10))
-    seven = db.Column(db.String(10))
-    eight = db.Column(db.String(10))
-    nine = db.Column(db.String(10))
-    ten = db.Column(db.String(10))
-    name = db.Column(db.String(20))
-    pin = db.Column(db.Integer, nullable=True)
-
-    def __init__(self, one, two, three, four, five, six, seven, eight, nine, ten, name):
-        self.one = one
-        self.two = two
-        self.three = three
-        self.four = four
-        self.five = five
-        self.six = six
-        self.seven = seven
-        self.eight = eight
-        self.nine = nine
-        self.ten = ten
-        self.name = name
-        db.session.add(self)
-        db.session.commit()
-
-    def __repr__(self):
-        return '<Map %r>' % self.map_id
-
-    def setPIN(self, pin: int) -> bool:
-        self.pin = pin
-        return True
-
-db.create_all()
-
+from controller.userManagement import redirectTeacher
+from controller.mapControl import createMap, isValidMap, makeChallenge, deleteMap, getGrid
 
 @app.route('/')
 def index():
     return render_template("landing.html")
 
+@app.route('/teacherdashboard')
+def teacherdashboard():
+    return redirectTeacher()
 
 @app.route('/createMap', methods=['GET', 'POST'])
 def createNewMap():
@@ -100,6 +27,10 @@ def createNewMap():
         else:
             redirect(url_for('createNewMap'))
 
+@app.route('/deleteMap/<id>')
+def delMap(id):
+    return deleteMap(id)
+
 
 @app.route('/createChallenge', methods=['POST'])
 def createNewChallenge():
@@ -113,13 +44,7 @@ def createNewChallenge():
 
 @app.route('/game/<game_id>')
 def game(game_id):
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    # c.row_factory = sqlite3.Row
-
-    c.execute("SELECT * FROM map WHERE map_id = ?", str(game_id))
-    game_map = c.fetchone()
-    return render_template("game_map.html", game_map=game_map)
+    return render_template("game_map.html", game_map=getGrid(game_id))
     
 
 if __name__ == "__main__":
