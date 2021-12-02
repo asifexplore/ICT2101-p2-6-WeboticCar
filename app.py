38cf4c07de8e46f6271a8a377d88e69645eeee5d
Asif Explore <mohamedasif167@gmail.com>
2021-12-02 19:25:10 +0800

    send hardcoded instructions to car completed

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import render_template, url_for, request, redirect
+from flask import render_template, url_for, request, redirect, jsonify
 from init import app
 from UserManagement import userLogin, addStudent, redirectDashboard
-
-
 
 @app.route('/')
 def index():
@@ -22,6 +20,19 @@ def register():
     addStudent(request.form['username'], request.form['password'])
     return redirectDashboard()
 
+@app.route('/getCarInstruction', methods = ['GET', 'POST'])
+def getCarInstruction():
+    return "123"
+
+@app.route('/setCarStatus', methods = ['GET', 'POST'])
+def setCarStatus():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+    return str(id)
+
 if __name__ == "__main__":
     context = ('cert.pem', 'key.pem')
-    app.run(ssl_context = context, debug = True)
+    app.run(debug=True, host="0.0.0.0", port="5000")
+    # app.run(ssl_context = context, debug = True)
