199dd4712951868e8fbd9eb598543505c53c53be
Ang Kok Hwee <71871315+Iciclemeltz@users.noreply.github.com>
2021-12-05 23:10:41 +0800

    Integration Update
    Updated the codes for the init.py to include the role column

--- init.py
+++ init.py
@@ -25,12 +25,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(80))
-#    role = db.Column(db.Integer)
+    role = db.Column(db.Integer)
     
-    def __init__(self, username, password):
+    def __init__(self, username, password, role):
         self.username = username
         self.password = password
-#        self.role = role
+        self.role = role
 
     def __repr__(self):
         return '<User %r>' % self.id
